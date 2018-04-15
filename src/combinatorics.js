8955c8bd03facca615cffea5ea9680b445b3c10f
leeyc0 <leeyc0@users.noreply.github.com>
2018-04-15 15:08:30 +0800

    improve combinatorics library

--- src/combinatorics.js
+++ src/combinatorics.js
@@ -1,82 +1,69 @@
 "use strict";
 
-function* distinglishableBallInBoxes(balls, numberOfBoxes) {
-  var counter=0;
-  var numberOfBalls = balls.length;
-  var maxCounter = numberOfBoxes**numberOfBalls;
-  
+function* distinglishableBallInBoxes(balls, boxes) {
+  let counter=0;
+  let numberOfBalls = balls.length;
+  let numberOfBoxes = boxes.length;
+  let maxCounter = numberOfBoxes**numberOfBalls;
   while (counter < maxCounter) {
-    let box = new Array(numberOfBoxes);
-    for (let i=0; i<numberOfBoxes; i++) {
-      box[i] = [];
+    let boxOutput = {};
+    for (let box of boxes) {
+      boxOutput[box] = [];
     }
     for (let i=0; i<numberOfBalls; i++) {
-      let ballInBox;
-      ballInBox = (Math.floor(counter / (numberOfBoxes**i))) % numberOfBoxes;
-      box[ballInBox].unshift(balls[numberOfBalls-1-i]);
+      let digit = (Math.floor(counter / (numberOfBoxes**i))) % numberOfBoxes;
+      let ballInBox = boxes[digit];
+      boxOutput[ballInBox].unshift(balls[numberOfBalls-1-i]);
     }
     counter++;
-    yield box;
+    yield boxOutput;
   }
 }
 
-function* indistinglishableBallInBoxes(numberOfBalls, numberOfBoxes) {
+function* indistinglishableBallInBoxes(numberOfBalls, boxes) {
   let i=0;
   let j=0;
-  let box = new Array(numberOfBoxes);
-  box[0] = numberOfBalls;
-  for (let i=1; i<numberOfBoxes; i++) {
-    box[i] = 0;
+  let numberOfBoxes = boxes.length;
+  let boxOutput = {};
+  for (let box of boxes) {
+    boxOutput[box] = 0;
   }
-  
-  i=0;
-  j=0;
+  boxOutput[boxes[0]] = numberOfBalls;
+
   while (true) {
-    yield box;
-    if (box[numberOfBoxes-1] === numberOfBalls) {
+    yield boxOutput;
+    if (boxOutput[boxes[numberOfBoxes-1]] === numberOfBalls) {
       break;
     }
     if (j === numberOfBoxes-1) {
       do {
         j--;
-      } while (box[j] === 0);
+      } while (boxOutput[boxes[j]] === 0);
       if (i === j) {
-        box[i]--;
+        boxOutput[boxes[i]]--;
         i++;
         j=i;
-        box[i] = box[numberOfBoxes-1] + 1;
+        boxOutput[boxes[i]] = boxOutput[boxes[numberOfBoxes-1]] + 1;
         if (i !== numberOfBoxes-1) {
-          box[numberOfBoxes-1] = 0;
+          boxOutput[boxes[numberOfBoxes-1]] = 0;
         }
       } else {
-        box[j]--;
+        boxOutput[boxes[j]]--;
         j++;
-        box[j] = box[numberOfBoxes-1] + 1;
+        boxOutput[boxes[j]] = boxOutput[boxes[numberOfBoxes-1]] + 1;
         if (j !== numberOfBoxes-1) {
-          box[numberOfBoxes-1] = 0;
+          boxOutput[boxes[numberOfBoxes-1]] = 0;
         }
       }
     } else {
-      box[j]--;
+      boxOutput[boxes[j]]--;
       j++;
-      box[j]++;
+      boxOutput[boxes[j]]++;
     }
   }
 }
 
-var a = [1,2,3];
-var numberOfBoxes = 3;
-
-
-
-for (var i of distinglishableBallInBoxes(a, numberOfBoxes)) {
-  for (var j of indistinglishableBallInBoxes(1, numberOfBoxes)) {
-    for (var k of indistinglishableBallInBoxes(1, numberOfBoxes)) {
-      console.log(i,j,k);
-    }
-  }
-}
-
-/*for (let i of indistinglishableBallInBoxes(4, 4)) {
-  console.log(i);
-}*/
+export default {
+  distinglishableBallInBoxes,
+  indistinglishableBallInBoxes,
+};
